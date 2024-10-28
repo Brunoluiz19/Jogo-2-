61196996b2a8ed95d7914a98b87b8ef02735b1bb
Brunoluiz19 <brunolp3@al.insper.edu.br>
2024-10-28 17:00:55 -0300

    movimento

--- jogo2.py
+++ jogo2.py
@@ -8,8 +8,6 @@ import os
 
 # Inicializa o Pygame
 pygame.init()
-pygame.mixer.init()
-
 
 # Configurações da tela
 largura_tela = 1000
@@ -28,12 +26,17 @@ x_quadrado = largura_tela // 2 - largura_quadrado // 2
 y_quadrado = altura_tela // 2 - altura_quadrado // 2
 velocidade = 5
 
+# Direção inicial
+direcao_x = velocidade
+direcao_y = 0
+
 # Configuração de FPS
 clock = pygame.time.Clock()
 FPS = 60
 
 # Função principal do jogo
 def jogo():
+    global x_quadrado, y_quadrado, direcao_x, direcao_y
     rodando = True
     while rodando:
         # Verifica eventos
@@ -41,27 +44,46 @@ def jogo():
             if event.type == pygame.QUIT:
                 rodando = False
 
-        # Captura as teclas pressionadas
+        # Captura as teclas pressionadas para mudar a direção
         teclas = pygame.key.get_pressed()
-        global x_quadrado, y_quadrado
-        
-        # Movimento do quadrado
-        if teclas[pygame.K_a] and x_quadrado > 0:
-            x_quadrado -= velocidade
-        if teclas[pygame.K_d] and x_quadrado < largura_tela - largura_quadrado:
-            x_quadrado += velocidade
-        if teclas[pygame.K_w] and y_quadrado > 0:
-            y_quadrado -= velocidade
-        if teclas[pygame.K_s] and y_quadrado < altura_tela - altura_quadrado:
-            y_quadrado += velocidade
-        if teclas[pygame.K_LEFT] and x_quadrado > 0:
-            x_quadrado -= velocidade
-        if teclas[pygame.K_RIGHT] and x_quadrado < largura_tela - largura_quadrado:
-            x_quadrado += velocidade
-        if teclas[pygame.K_UP] and y_quadrado > 0:
-            y_quadrado -= velocidade
-        if teclas[pygame.K_DOWN] and y_quadrado < altura_tela - altura_quadrado:
-            y_quadrado += velocidade
+        if teclas[pygame.K_a]:  # Esquerda
+            direcao_x = -velocidade
+            direcao_y = 0
+        elif teclas[pygame.K_d]:  # Direita
+            direcao_x = velocidade
+            direcao_y = 0
+        elif teclas[pygame.K_w]:  # Cima
+            direcao_x = 0
+            direcao_y = -velocidade
+        elif teclas[pygame.K_s]:  # Baixo
+            direcao_x = 0
+            direcao_y = velocidade
+        if teclas[pygame.K_LEFT]:  # Esquerda
+            direcao_x = -velocidade
+            direcao_y = 0
+        elif teclas[pygame.K_RIGHT]:  # Direita
+            direcao_x = velocidade
+            direcao_y = 0
+        elif teclas[pygame.K_UP]:  # Cima
+            direcao_x = 0
+            direcao_y = -velocidade
+        elif teclas[pygame.K_DOWN]:  # Baixo
+            direcao_x = 0
+            direcao_y = velocidade
+
+        # Movimento do quadrado com limite de bordas
+        x_quadrado += direcao_x
+        y_quadrado += direcao_y
+
+        # Restringe o quadrado às bordas da tela
+        if x_quadrado < 0:
+            x_quadrado = 0
+        elif x_quadrado > largura_tela - largura_quadrado:
+            x_quadrado = largura_tela - largura_quadrado
+        if y_quadrado < 0:
+            y_quadrado = 0
+        elif y_quadrado > altura_tela - altura_quadrado:
+            y_quadrado = altura_tela - altura_quadrado
 
         # Limpa a tela
         tela.fill(BRANCO)
